bf7fe4efd02c242254bf0552d815cf32495a26ae
reece-vlr <reece.vlr@gmail.com>
2023-08-21 16:44:24 +0800

    Fix password verification, add button lock

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const pwd = document.querySelector('#user-password');
 const confirmPwd = document.querySelector('#user-password-confirm');
+
+const pwdField = document.querySelectorAll('.pwd');
 const btn = document.querySelector('#submit');
 
-confirmPwd.addEventListener('keyup', function(e) {
+pwdField.forEach(password => {
+    password.addEventListener('keyup', verify);
+});
+
+function verify() {
+    // verify if password is !empty
+    if (pwd.value !== '') {
+        confirmPwd.disabled = false;
+    }
+
     if (confirmPwd.value !== pwd.value) {
+        confirmPwd.classList.remove('verified');
+
         confirmPwd.classList.add('error');
-        // confirmPwd.setAttribute('isValid', false);
     }
     else {
         confirmPwd.classList.remove('error');
-        // confirmPwd.setAttribute('isValid', true);
+
+        confirmPwd.classList.add('verified');
+        btn.disabled = false;
     }
-});
+}
